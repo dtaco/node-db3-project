6cec94323a5a4c342e4038833bdd6a9e9ff3e300
David Tacoronte <d.tacoronte89@gmail.com>
2024-01-04 12:20:26 -0500

    started on model updated find

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -33,15 +33,6 @@ const validateScheme = (req, res, next) => {
   }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
   const {instructions, step_number } = req.body
 
